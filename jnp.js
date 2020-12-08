c63649f043c4bb6fa32969baea0a195baec4a3bb
Any <10735897+anyxf@users.noreply.github.com>
2020-12-08 21:53:58 +0700

    Update jnp.js

--- jnp.js
+++ jnp.js
@@ -1,3 +1,3 @@
-$(() => {
-        $("#np1").load("https://cdn.statically.io/gh/anyxf/safence/main/el.html");
-    })();
+$(document).ready(function () {
+    $("#np1").load("https://cdn.statically.io/gh/anyxf/safence/main/el.html");
+});
